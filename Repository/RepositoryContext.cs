1fcb1875064fbc53f1a6ae25804efaa660018c88
arunpratap26 <arunpratap26@yahoo.com>
2024-07-18 00:04:39 +0530

    added repository project and created RepositoryContext class and RepositoryContextFactory class

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
