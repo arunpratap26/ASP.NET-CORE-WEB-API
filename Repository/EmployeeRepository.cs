0ad51f68a5c79722fb8591bbbfd1be1f8c04b366
arunpratap26 <arunpratap26@yahoo.com>
2024-07-20 20:29:02 +0530

    added repository user interfaces and classes

---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+    }
+}
