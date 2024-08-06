efec5e68fb7c7345d1c76f5747eb706c4b707409
arunpratap26 <arunpratap26@yahoo.com>
2024-08-06 15:17:39 +0530

    Implimented searching

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -2,6 +2,7 @@
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using Shared.RequestFeatures;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -15,9 +16,11 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
-                            .OrderBy(e => e.Name)
-                            .ToListAsync();
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),trackChanges)
+                                                 .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+                                                 .Search(employeeParameters.SearchTerm)
+                                                 .OrderBy(e => e.Name)
+                                                 .ToListAsync();
 
             return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
         }
--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -5,5 +5,6 @@
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
+        public string? SearchTerm { get; set; }
     }
 }
